db703a4801a87b807ca3e0a124a074c075b3aa25
Getbuuf <alesabokun7571@gmail.com>
2024-03-22 12:35:11 +0300

    practic_1

--- practic_1.js
+++ practic_1.js
@@ -1,6 +1,30 @@
-let A = [7, 9, -2];
-let B = [5, 4, 3];
-let K = -0.5;
+const c = [1, 0, 1];
+const b = [-2, 3, 5];
 
-// задание 1//
-console.log(A+B);
+// задание 1
+const sum = c.map((x, i) => x + b[i]);
+
+console.log("c + b:", sum); // [-1, 3, 6]
+
+// задание 2
+const difference = c.map((x, i) => x - b[i]);
+
+console.log("c - b:", difference); // [3, -3, -4]
+
+// задание 3
+const dotProduct = c.reduce((acc, x, i) => acc + x * b[i], 0);
+
+console.log("c * b:", dotProduct); // 8
+
+// задание 4
+const cMagnitude = Math.sqrt(c.reduce((acc, x) => acc + x**2, 0));
+const bMagnitude = Math.sqrt(b.reduce((acc, x) => acc + x**2, 0));
+
+console.log("|c|:", cMagnitude); // Math.sqrt(2)
+console.log("|b|:", bMagnitude); // Math.sqrt(34)
+
+// задание 5
+const scaledVector = c.map((x) => x * k);
+
+console.log("k * c:", scaledVector); // [3.5, 0, 3.5]
+
--- practic_1.py
+++ practic_1.py
@@ -4,8 +4,3 @@ A = np.array([7,9,-2])
 B = np.array([5,4,3])
 K = -0.5
 
-# задание 1
-print(A+B)
-
-# задание 2
-print(A*B)
