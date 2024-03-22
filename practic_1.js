c53f53961bbd729bce808fecf943b5b99e2ea6c7
Getbuuf <alesabokun7571@gmail.com>
2024-03-22 12:04:31 +0300

    practic_1

--- practic_1.js
+++ practic_1.js
@@ -1,3 +1,6 @@
 let A = [7, 9, -2];
 let B = [5, 4, 3];
-let K = -0,5
+let K = -0.5;
+
+// задание 1//
+console.log(A+B);
---
+++ practic_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+A = np.array([7,9,-2])
+B = np.array([5,4,3])
+K = -0.5
+
+# задание 1
+print(A+B)
+
+# задание 2
+print(A*B)
