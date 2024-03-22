38cb68f17016ff0f8b5ff034f40e71f6f8c85673
Лев <115810918+Leo-Volkov@users.noreply.github.com>
2024-03-22 14:11:47 +0300

    Update practic_1.js

--- practic_1.js
+++ practic_1.js
@@ -5,12 +5,12 @@ const k = 3.5;
 // задание 1
 const sum = c.map((x, i) => x + b[i]);
 
-console.log("Задание 1:", sum); // [-1, 3, 6]
+console.log("Задание 1: ", sum); // [-1, 3, 6]
 
 // задание 2
 const difference = c.map((x, i) => x - b[i]);
 
-console.log("Задание 2:", difference); // [3, -3, -4]
+console.log("Задание 2: ", difference); // [3, -3, -4]
 
 // задание 3
 let result3 = [];
@@ -18,13 +18,13 @@ for (let i = 0; i < c.length; i++) {
     result3.push(c[i] * b[i]);
 }
 
-console.log("Задание 3:", result3); // [-2, 0, 5]
+console.log("Задание 3: ", result3); // [-2, 0, 5]
 
 // задание 4
 const cMagnitude = Math.sqrt(c.reduce((acc, x) => acc + x**2, 0));
 const bMagnitude = Math.sqrt(b.reduce((acc, x) => acc + x**2, 0));
 
-console.log("Задание 4:", cMagnitude * bMagnitude); // 8.717797887081348
+console.log("Задание 4: ", cMagnitude * bMagnitude); // 8.717797887081348
 
 console.log();
 
@@ -34,4 +34,4 @@ let result5 = []
 for (let i = 0; i < c.length; i++) {
     result5.push(k * c[i]);
 }
-console.log("Задание 5:", result5); //[3.5, 0, 3.5]
+console.log("Задание 5: ", result5); //[3.5, 0, 3.5]
