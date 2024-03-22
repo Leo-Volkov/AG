831077bb7ea41f1d09b1186962afd4c1164b16cd
Leo-Volkov <Leo.Volkov156@yandex.ru>
2024-03-22 13:27:44 +0300

    practic_1

--- practic_1.js
+++ practic_1.js
@@ -5,23 +5,26 @@ const k = 3.5;
 // задание 1
 const sum = c.map((x, i) => x + b[i]);
 
-console.log("c + b:", sum); // [-1, 3, 6]
+console.log("Задание 1:", sum); // [-1, 3, 6]
 
 // задание 2
 const difference = c.map((x, i) => x - b[i]);
 
-console.log("c - b:", difference); // [3, -3, -4]
+console.log("Задание 2:", difference); // [3, -3, -4]
 
 // задание 3
-// const dotProduct = c.reduce((acc, x, i) => acc + x * b[i], 0);
+const result = [];
+for (let i = 0; i < c.length; i++) {
+    result.push(c[i] * b[i]);
+}
 
-// console.log("c * b:", dotProduct); // 8
+console.log("Задание 3:", result); // [-2, 0, 5]
 
 // задание 4
 const cMagnitude = Math.sqrt(c.reduce((acc, x) => acc + x**2, 0));
 const bMagnitude = Math.sqrt(b.reduce((acc, x) => acc + x**2, 0));
 
-console.log("c * b:", cMagnitude * bMagnitude); // 8.717797887081348
+console.log("Задание 4:", cMagnitude * bMagnitude); // 8.717797887081348
 
 // задание 5
 // const scaledVector = c.map((x) => x * k);
