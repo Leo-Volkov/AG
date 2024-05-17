db2e83a29a2e5afeb56ef49b63d9537aff8861a9
Getbuuf <alesabokun7571@gmail.com>
2024-05-17 11:46:52 +0300

    practic_12

---
+++ графики/practic_12.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [25, 32, 34, 20, 25]
+plt.plot(x, y)
+plt.xlabel('Ось абцисс')
+plt.ylabel('Ось ординат')
+plt.title('График по точкам')
+plt.show()
